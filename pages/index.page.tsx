f3d02a1e0b4aef0047eac7cb4d3b0fcebbca1ff5
Nik Jmaeff <nik@jmaeff.me>
2022-05-16 08:05:43 -0700

    add template editable table

--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -1,11 +1,13 @@
 import {Meta} from "./lib/meta";
 import {Page} from "./lib/page";
+import {EditableTable} from "./lib/table";
 
 export default () => {
 
     return <>
         <Meta title={'Inventory'}/>
         <Page>
+            <EditableTable/>
         </Page>
     </>
 };
